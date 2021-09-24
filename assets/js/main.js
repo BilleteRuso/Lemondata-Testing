33ffc2d69d3bbe24c389fe04be94ed4b26f695b7
Nicolás Demaria <nicolasdemaria123@gmail.com>
2021-09-24 01:28:28 -0300

    First mock-up 0.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,5 +204,21 @@
       }
     }
   });
+  
+    /**
+   * Animation on scroll
+   */
+    window.addEventListener('load', () => {
+      AOS.init({
+        duration: 1000,
+        easing: "ease-in-out",
+        once: true,
+        mirror: false
+      });
+    });
+    
 
 })()
+
+
+
